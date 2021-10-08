cf1aa233d63302350d1b011ebe9beaafa7accd19
lucas-ssv <lucas_99silva@hotmail.com>
2021-10-08 14:57:47 -0300

    feat: add new page Home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,58 @@
+import { useContext } from "react";
+import { FiLogIn, FiSearch } from "react-icons/fi";
+import { Header } from "../components/Header";
+import { Modal } from "../components/Modal";
+import { ModalContext } from "../contexts/ModalContext";
+import { AsyncSelect } from "../components/AsyncSelect";
+import { Button } from "../components/Button";
+import Background from "../assets/Background.svg";
+import Pessoas from "../assets/Pessoas.svg";
+import styles from "../styles/home.module.scss";
+import "../styles/common.scss";
+
+export function Home() {
+  const { openModal } = useContext(ModalContext);
+
+  return (
+    <>
+      <Modal>
+        <h1>Pontos de coleta</h1>
+
+        <AsyncSelect placeholder="Digite a cidade" />
+        <AsyncSelect placeholder="Digite a estado" />
+        <Button type="button" text="Buscar" />
+      </Modal>
+
+      <main className="homeContainer">
+        <Header
+          Icon={<FiLogIn size={24} color="#34cb79" />}
+          path="/register"
+          text="Cadastre um ponto de coleta"
+        />
+        <section className={styles.homeContent}>
+          <img
+            src={Background}
+            className={styles.background}
+            alt="background"
+          />
+
+          <div>
+            <h1>Seu marketplace de coleta de resíduos.</h1>
+            <p>
+              Ajudamos pessoas a encontrarem pontos de coleta de forma
+              eficiente.
+            </p>
+            <button type="button" onClick={openModal}>
+              <span>
+                <FiSearch size={24} color="#fff" />
+              </span>
+              <strong>Pesquisar pontos de coleta</strong>
+            </button>
+          </div>
+
+          <img src={Pessoas} alt="pessoas" />
+        </section>
+      </main>
+    </>
+  );
+}
