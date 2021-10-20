294347ec0802381bb785bbc5484430abb189253e
lucas-ssv <lucas_99silva@hotmail.com>
2021-10-20 18:20:47 -0300

    feat: add makeServer mirage on development

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Modal from "react-modal";
 import { App } from "./App";
+import { makeServer } from "./services/mirage";
 import "./styles/global.scss";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
 
 Modal.setAppElement("#root");
 
