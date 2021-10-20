6eb8014d2fb4307b9f01a55709612a6996651b70
lucas-ssv <lucas_99silva@hotmail.com>
2021-10-20 18:18:51 -0300

    feat: add validation and errors message

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,15 +1,23 @@
-import { InputHTMLAttributes } from "react";
+import { forwardRef, InputHTMLAttributes } from "react";
+import { FieldError } from "react-hook-form";
+import { FiAlertCircle } from "react-icons/fi";
 import styles from "./input.module.scss";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  error: FieldError | undefined;
 }
 
-export function Input({ label, ...rest }: InputProps) {
-  return (
-    <label className={styles.label} htmlFor={label}>
-      {label}
-      <input {...rest} />
-    </label>
-  );
-}
+export const Input = forwardRef(
+  ({ label, error, ...rest }: InputProps, ref: any) => {
+    return (
+      <label className={styles.label} htmlFor={label} ref={ref}>
+        {label}
+        <input {...rest} />
+        {error && (
+          <FiAlertCircle size={20} color="#e91e63" title={error.message} />
+        )}
+      </label>
+    );
+  }
+);
