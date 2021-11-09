72d23d294b2f394699c1ed4736456fa2dcb2d2ba
lucas-ssv <lucas_99silva@hotmail.com>
2021-11-09 17:52:19 -0300

    feat: add dynamic options to select

--- src/components/AsyncSelect/index.tsx
+++ src/components/AsyncSelect/index.tsx
@@ -1,47 +1,47 @@
+import { forwardRef } from "react";
 import ReactAsyncSelect from "react-select/async";
 import { customStyles } from "./customStyles";
 
 interface AsyncSelectProps {
+  items: {
+    label: string;
+    value: string;
+  }[];
   placeholder?: string;
 }
 
-const cities = [
-  { label: "São Paulo", value: "São Paulo" },
-  { label: "Rio de Janeiro", value: "Rio de Janeiro" },
-  { label: "Sorocaba", value: "Sorocaba" },
-  { label: "Osasco", value: "Osasco" },
-  { label: "Ribeirão Preto", value: "Ribeirão Preto" },
-];
+export const AsyncSelect = forwardRef(
+  ({ items, placeholder }: AsyncSelectProps, ref: any) => {
+    const filterCities = (inputValue: string) => {
+      return items.filter((city) =>
+        city.label.toLowerCase().includes(inputValue.toLowerCase())
+      );
+    };
 
-export function AsyncSelect({ placeholder }: AsyncSelectProps) {
-  const filterCities = (inputValue: string) => {
-    return cities.filter((city) =>
-      city.label.toLowerCase().includes(inputValue.toLowerCase())
+    const loadOptions = (
+      inputValue: string,
+      callback: (options: any) => void
+    ) => {
+      setTimeout(() => {
+        callback(filterCities(inputValue));
+      }, 1000);
+    };
+
+    function handleInputChange(newValue: string) {
+      const inputValue = newValue.replace(/\W/g, "");
+      return inputValue;
+    }
+
+    return (
+      <ReactAsyncSelect
+        styles={customStyles}
+        cacheOptions
+        loadOptions={loadOptions}
+        defaultOptions
+        onInputChange={handleInputChange}
+        placeholder={placeholder}
+        ref={ref}
+      />
     );
-  };
-
-  const loadOptions = (
-    inputValue: string,
-    callback: (options: any) => void
-  ) => {
-    setTimeout(() => {
-      callback(filterCities(inputValue));
-    }, 1000);
-  };
-
-  function handleInputChange(newValue: string) {
-    const inputValue = newValue.replace(/\W/g, "");
-    return inputValue;
   }
-
-  return (
-    <ReactAsyncSelect
-      styles={customStyles}
-      cacheOptions
-      loadOptions={loadOptions}
-      defaultOptions
-      onInputChange={handleInputChange}
-      placeholder={placeholder}
-    />
-  );
-}
+);
